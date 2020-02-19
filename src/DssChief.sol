b46fc72f55388b23c0d0087bef2b1dca4e84276b
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2020-02-19 11:51:56 -0300

    Initial commit

--- src/DssChief.sol
+++ src/DssChief.sol
@@ -1,4 +1,90 @@
-pragma solidity ^0.5.15;
+pragma solidity ^0.5.12;
 
-contract DssChief {
+import { DSAuth, DSAuthority } from "ds-auth/auth.sol";
+
+contract TokenLike {
+    function transferFrom(address, address, uint256) external;
+    function transfer(address, uint256) external;
+}
+
+contract DssChief is DSAuth, DSAuthority {
+    TokenLike                       public gov;         // MKR gov token
+    uint256                         public supply;      // Total MKR locked
+    uint256                         public ttl;         // MKR locked expiration time (admin param)
+    address                         public hat;         // Elected Candidate
+    mapping(address => address)     public votes;       // Voter => Candidate
+    mapping(address => uint256)     public approvals;   // Candidate => Amount of votes
+    mapping(address => uint256)     public deposits;    // Voter => Voting power
+    mapping(address => uint256)     public last;        // Last time executed
+
+    function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x + y) >= x);
+    }
+
+    function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require((z = x - y) <= x);
+    }
+
+    constructor(address gov_) public {
+        gov = TokenLike(gov_);
+    }
+
+    function file(bytes32 what, uint256 data) public auth {
+        if (what == "ttl") ttl = data;
+        else revert("DssChief/file-unrecognized-param");
+    }
+
+    function lock(uint256 wad) external {
+        // Pull collateral from sender's wallet
+        gov.transferFrom(msg.sender, address(this), wad);
+        // Increase amount deposited from sender
+        deposits[msg.sender] = add(deposits[msg.sender], wad);
+        // Add new voting power to the actual voted candidate
+        approvals[votes[msg.sender]] = add(approvals[votes[msg.sender]], wad);
+        // Increase total supply
+        supply = add(supply, wad);
+        // Signal this account has been active and renew expiration time
+        last[msg.sender] = now;
+    }
+
+    function free(address usr, uint256 wad) external {
+        // Verify usr is sender or their voting power is already expired
+        require(usr == msg.sender || add(last[usr], ttl) < now , "DssChief/not-allowed-to-free");
+        // Verify is not freeing on same block where another action happened (to avoid usage of flash loans)
+        require(last[usr] < now, "DssChief/not-minimum-time-passed");
+        // Decrease amount deposited from usr
+        deposits[usr] = sub(deposits[usr], wad);
+        // Remove voting power from the actual voted candidate
+        approvals[votes[usr]] = sub(approvals[votes[usr]], wad);
+        // Decrease total supply
+        supply = sub(supply, wad);
+        // Push token back to usr's wallet
+        gov.transfer(usr, wad);
+        // Signal this account has been active and renew expiration time
+        last[msg.sender] = now;
+    }
+
+    function vote(address whom) external {
+        // Remove voting power from the actual voted candidate
+        approvals[votes[msg.sender]] = sub(approvals[votes[msg.sender]], deposits[msg.sender]);
+        // Vote new candidate
+        votes[msg.sender] = whom;
+        // Add new voting power to the new candidate
+        approvals[whom] = add(approvals[whom], deposits[msg.sender]);
+        // Signal this account has been active and renew expiration time
+        last[msg.sender] = now;
+    }
+
+    function lift(address whom) external {
+        // Verify the actual candidate has more than half of total supply of locked MKR
+        require(approvals[whom] > supply / 2, "not-enough-voting-power");
+        // Elect new candidate
+        hat = whom;
+        // Signal this account has been active and renew expiration time
+        last[msg.sender] = now;
+    }
+
+    function canCall(address caller, address, bytes4) public view returns (bool ok) {
+        ok = caller == hat;
+    }
 }
--- src/DssChief.t.sol
+++ src/DssChief.t.sol
@@ -1,21 +1,239 @@
-pragma solidity ^0.5.15;
+pragma solidity ^0.5.12;
 
 import "ds-test/test.sol";
+import { DSToken } from "ds-token/token.sol";
 
-import "./DssChief.sol";
+import { DSAuth, DSAuthority, DssChief } from "./DssChief.sol";
 
-contract DssChiefTest is DSTest {
+contract Hevm {
+    function warp(uint) public;
+}
+
+contract ChiefUser {
     DssChief chief;
 
+    constructor(DssChief chief_) public {
+        chief = chief_;
+    }
+
+    function doTransferFrom(DSToken token, address from, address to,
+                            uint amount)
+        public
+        returns (bool)
+    {
+        return token.transferFrom(from, to, amount);
+    }
+
+    function doTransfer(DSToken token, address to, uint amount)
+        public
+        returns (bool)
+    {
+        return token.transfer(to, amount);
+    }
+
+    function doApprove(DSToken token, address recipient, uint amount)
+        public
+        returns (bool)
+    {
+        return token.approve(recipient, amount);
+    }
+
+    function doAllowance(DSToken token, address owner, address spender)
+        public view
+        returns (uint)
+    {
+        return token.allowance(owner, spender);
+    }
+
+    function doVote(address whom) public {
+        chief.vote(whom);
+    }
+
+    function doLift(address whom) public {
+        chief.lift(whom);
+    }
+
+    function doLock(uint wad) public {
+        chief.lock(wad);
+    }
+
+    function doFree(ChiefUser usr, uint wad) public {
+        chief.free(address(usr), wad);
+    }
+}
+
+contract CandidateUser {
+    System sys;
+
+    constructor(System sys_) public {
+        sys = sys_;
+    }
+
+    function doSysTest() public {
+        sys.test();
+    }
+}
+
+contract System is DSAuth {
+    function test() public auth {}
+}
+
+contract DssChiefTest is DSTest {
+    uint256 constant user1InitialBalance = 350 ether;
+    uint256 constant user2InitialBalance = 250 ether;
+    uint256 constant user3InitialBalance = 200 ether;
+
+    Hevm hevm;
+
+    DssChief chief;
+    DSToken gov;
+
+    ChiefUser user1;
+    ChiefUser user2;
+    ChiefUser user3;
+
+    address candidate1;
+    address candidate2;
+    address candidate3;
+
+    System sys; // Mocked System to authed via chief
+
     function setUp() public {
-        chief = new DssChief();
+        // init hevm
+        hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+        hevm.warp(0);
+
+        gov = new DSToken("GOV");
+        gov.mint(1000 ether);
+
+        chief = new DssChief(address(gov));
+        chief.file("ttl", 100);
+
+        sys = new System();
+        sys.setAuthority(DSAuthority(address(chief)));
+
+        user1 = new ChiefUser(chief);
+        user2 = new ChiefUser(chief);
+        user3 = new ChiefUser(chief);
+
+        candidate1 = address(new CandidateUser(sys));
+        candidate2 = address(new CandidateUser(sys));
+        candidate3 = address(new CandidateUser(sys));
+
+        gov.transfer(address(user1), user1InitialBalance);
+        gov.transfer(address(user2), user2InitialBalance);
+        gov.transfer(address(user3), user3InitialBalance);
+
+        user3.doApprove(gov, address(chief), uint(-1));
+        user2.doApprove(gov, address(chief),  uint(-1));
+        user1.doApprove(gov, address(chief),  uint(-1));
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function test_lock_debits_user() public {
+        assertEq(gov.balanceOf(address(user1)), user1InitialBalance);
+
+        uint lockedAmt = user1InitialBalance / 10;
+        user1.doApprove(gov, address(chief), lockedAmt);
+        user1.doLock(lockedAmt);
+
+        assertEq(gov.balanceOf(address(user1)), user1InitialBalance - lockedAmt);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function test_free() public {
+        uint user1LockedAmt = user1InitialBalance / 2;
+        user1.doApprove(gov, address(chief), user1LockedAmt);
+        user1.doLock(user1LockedAmt);
+        assertEq(gov.balanceOf(address(user1)), user1InitialBalance - user1LockedAmt);
+        hevm.warp(1);
+        user1.doFree(user1, user1LockedAmt);
+        assertEq(gov.balanceOf(address(user1)), user1InitialBalance);
+    }
+
+    function testFail_free_same_time() public {
+        uint user1LockedAmt = user1InitialBalance / 2;
+        user1.doApprove(gov, address(chief), user1LockedAmt);
+        user1.doLock(user1LockedAmt);
+        user1.doFree(user1, user1LockedAmt);
+    }
+
+    function test_changing_weight_after_voting() public {
+        uint user1LockedAmt = user1InitialBalance / 2;
+        user1.doLock(user1LockedAmt);
+
+        user1.doVote(candidate1);
+
+        assertEq(chief.approvals(candidate1), user1LockedAmt);
+
+        // Changing weight should update the weight of our candidate.
+        hevm.warp(1);
+        user1.doFree(user1, user1LockedAmt);
+        assertEq(chief.approvals(candidate1), 0);
+
+        user1LockedAmt = user1InitialBalance / 4;
+        user1.doLock(user1LockedAmt);
+
+        assertEq(chief.approvals(candidate1), user1LockedAmt);
+    }
+
+    function test_basic_lift_sys_test() public {
+        user2.doLock(user2InitialBalance);
+
+        user2.doVote(candidate1);
+        chief.lift(candidate1);
+        assertEq(candidate1, chief.hat());
+        CandidateUser(candidate1).doSysTest();
+    }
+
+    function testFail_sys_test_not_elected() public {
+        CandidateUser(candidate1).doSysTest();
+    }
+
+    function _tryLift(address usr) internal returns (bool ok) {
+        (ok,) = address(chief).call(abi.encodeWithSignature("lift(address)", usr));
+    }
+
+    function test_lift() public {
+        user3.doLock(user3InitialBalance);
+        user2.doLock(user2InitialBalance);
+        user1.doLock(user1InitialBalance);
+
+        user1.doVote(candidate1);
+        user2.doVote(candidate2);
+        user3.doVote(candidate3);
+        assertEq(chief.approvals(candidate1), 350 ether);
+        assertEq(chief.approvals(candidate2), 250 ether);
+        assertEq(chief.approvals(candidate3), 200 ether);
+        assertTrue(!_tryLift(candidate1)); // candidate1 ~ 43% => can't be elected
+
+        user3.doVote(candidate2);
+        assertEq(chief.approvals(candidate1), 350 ether);
+        assertEq(chief.approvals(candidate2), 450 ether);
+        assertEq(chief.approvals(candidate3), 0);
+        assertTrue(_tryLift(candidate2)); // candidate2 ~ 56% => can be elected
+
+        hevm.warp(1);
+        user3.doFree(user3, 100 ether);
+        assertEq(chief.approvals(candidate1), 350 ether);
+        assertEq(chief.approvals(candidate2), 350 ether);
+        assertEq(chief.approvals(candidate3), 0);
+        assertTrue(!_tryLift(candidate2)); // candidate2 = 50% => can't be elected
+
+        user3.doLock(1);
+        assertEq(chief.approvals(candidate1), 350 ether);
+        assertEq(chief.approvals(candidate2), 350 ether + 1);
+        assertEq(chief.approvals(candidate3), 0);
+        assertTrue(_tryLift(candidate2)); // candidate2 > 50% => can be elected
+    }
+
+    function test_free_other_user() public {
+        user3.doLock(user3InitialBalance);
+        hevm.warp(chief.ttl() + 1);
+        user1.doFree(user3, user3InitialBalance);
+    }
+
+    function testFail_free_other_user_not_time_passed() public {
+        user3.doLock(user3InitialBalance);
+        hevm.warp(chief.ttl());
+        user1.doFree(user3, user3InitialBalance);
     }
 }
